d90906f1ea204b66b023f29e15e7c5cb40cf341f
bahti <rasulovbahti@gmail.com>
2022-05-29 20:58:52 -0400

    testing

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/logout',(req,res) => {
 
 PORT = process.env.PORT | 3000
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running at port ${PORT}`)
+    console.log(`Bakhti Server running at port ${PORT}`)
 })
 
 
