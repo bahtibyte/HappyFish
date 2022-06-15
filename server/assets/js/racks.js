666c4999441c6e86073f6813917dff25899fab9a
bahti <rasulovbahti@gmail.com>
2022-06-15 18:16:30 -0400

    rack options funtionality

--- server/assets/js/racks.js
+++ server/assets/js/racks.js
@@ -1,9 +1,16 @@
 
-
 const newRack = function() {
-    alert('new rack')
+    fetch('/config/rack', { method: 'POST' })
+        .then(response => response.json())
+        .then(response => loadNewRack(response))
+        .catch(err => alert('unable to create new pwm '+err))
+    return false
 }
 
+const loadNewRack = function(rack) {
+    const root = create_rack(rack, null)
+    rack_div.appendChild(root)
+}
 
 const loadRacks = function() {
     fetch('/api/config', { method: 'GET' })
@@ -18,13 +25,13 @@ const load = function(config) {
 
     const racks = config['racks']
     for (var i = 0; i < racks.length; i++) {
-        const root = create_rack(i, config[racks[i]], config)
+        const root = create_rack(config[racks[i]], config)
         rack_div.appendChild(root)
     }
     last_config = config
 }
 
-const create_rack = function(rI, rack, config) {
+const create_rack = function(rack, config) {
 
     const rackId = rack['_id']
 
@@ -67,7 +74,7 @@ const create_rack = function(rI, rack, config) {
     edit_btn.setAttribute('class', 'btn btn-warning')
     edit_btn.setAttribute('style', 'float: right')
     edit_btn.setAttribute('id', rackId+'-edit-btn')
-    edit_btn.onclick = function() { alert('edit rack') }
+    edit_btn.onclick = function() { editRack(rack) }
     edit_btn.innerHTML = 'Edit'
     options_div.appendChild(edit_btn)
 
@@ -75,13 +82,13 @@ const create_rack = function(rI, rack, config) {
     add_btn.setAttribute('class', 'btn btn-primary')
     add_btn.setAttribute('style', 'float: left')
     add_btn.setAttribute('id', rackId+'-add-btn')
-    add_btn.onclick = function() { alert('add shelf') }
+    add_btn.onclick = function() { addNewShelf(rackId, body, options_div) }
     add_btn.innerHTML = '+ Shelf'
     options_div.appendChild(add_btn)
 
     const shelves = rack['shelves']
     for (var i = 0; i < shelves.length; i++) {
-        const shelf_root = create_shelf(shelves[i], config)
+        const shelf_root = create_shelf(config[shelves[i]])
         body.appendChild(shelf_root)
     }
 
@@ -98,9 +105,9 @@ const create_rack = function(rI, rack, config) {
 }
 
 
-const create_shelf = function(shelfId, config) {
+const create_shelf = function(shelf) {
     
-    const shelf = config[shelfId]
+    const shelfId = shelf['_id']
 
     const root = document.createElement('div')
     root.setAttribute('class', 'col-auto mb-3')
@@ -113,7 +120,7 @@ const create_shelf = function(shelfId, config) {
     card.setAttribute('id', shelfId+'-card')
 
     const body = document.createElement('div')
-    body.setAttribute('class', 'card-body')
+    body.setAttribute('class', 'card-body p-0')
     body.setAttribute('id', shelfId+'-body')
 
     const header = document.createElement('div')
@@ -139,14 +146,15 @@ const create_shelf = function(shelfId, config) {
 
     const configure_btn = document.createElement('button')
     configure_btn.setAttribute('class', 'btn btn-link')
-    configure_btn.setAttribute('style', 'float: right')
+    configure_btn.setAttribute('style', 'float: right;')
     configure_btn.setAttribute('id', shelfId+'-edit-btn')
-    configure_btn.onclick = function() { alert('edit shelf') }
+    configure_btn.onclick = function() { alert('configure shelf') }
     configure_btn.innerHTML = 'Configure'
     options_div.appendChild(configure_btn)
 
 
     const shelf_div = generateShelf(shelf)
+
     body.appendChild(shelf_div)
     body.appendChild(options_div)
 
@@ -175,15 +183,115 @@ const tbdShelf = function(shelf) {
 
     const div = document.createElement('div')
     div.setAttribute('class', 'border border-danger')
-    div.setAttribute('style', 'margin-bottom: 1rem')
+    div.setAttribute('style', 'margin: 1rem 1rem 1rem 1rem')
+    div.setAttribute('id', shelf['_id']+'-shelf-body-div')
 
     const p = document.createElement('p')
     p.setAttribute('class', 'unconfigured')
+    p.setAttribute('id', shelf['_id']+'-shelf-p')
     p.innerHTML = 'Unconfigured Shelf'
     
-
     div.appendChild(p)
 
     return div
+}
+
+const editRack = function(rack) {
+
+    const rackId = rack['_id']
+    
+    const options_div = document.getElementById(rackId+'-options-div')
+    const edit_btn = document.getElementById(rackId+'-edit-btn')
+    const add_btn = document.getElementById(rackId+'-add-btn')
+
+    options_div.removeChild(edit_btn)
+    options_div.removeChild(add_btn)
+
+    const save_btn = document.createElement('button')
+    save_btn.setAttribute('class', 'btn btn-success')
+    save_btn.setAttribute('style', 'float: right')
+    save_btn.setAttribute('id', rackId+'-save-btn')
+    save_btn.onclick = function() { alert('save') }
+    save_btn.innerHTML = 'Save'
+    options_div.appendChild(save_btn)
+
+    const cancel_btn = document.createElement('button')
+    cancel_btn.setAttribute('class', 'btn btn-secondary')
+    cancel_btn.setAttribute('style', 'float: right; margin-right: 10px')
+    cancel_btn.setAttribute('id', rackId+'-cancel-btn')
+    cancel_btn.onclick = function() { alert('cancel') }
+    cancel_btn.innerHTML = 'Cancel'
+    options_div.appendChild(cancel_btn)
+
+    const delete_btn = document.createElement('button')
+    delete_btn.setAttribute('class', 'btn btn-danger')
+    delete_btn.setAttribute('style', 'float: left; margin-right: 10px')
+    delete_btn.setAttribute('id', rackId+'-delete-btn')
+    delete_btn.onclick = function() { deleteRack(rack) }
+    delete_btn.innerHTML = 'Delete'
+    options_div.appendChild(delete_btn)
+
+    const header = document.getElementById(rackId+'-header')
+    const h5 = document.getElementById(rackId+'-header-h5')
+    header.removeChild(h5)
+
+    const input_div = document.createElement('div')
+    input_div.setAttribute('id', rackId+'-input-div')
+    input_div.setAttribute('class', 'input-group')
+
+    const prepend_div = document.createElement('div')
+    prepend_div.setAttribute('class', 'input-group-prepend')
+
+    const span = document.createElement('span')
+    span.setAttribute('class', 'input-group-text')
+    span.setAttribute('id', rackId+'-input-span')
+    span.innerHTML = '[Rack]: '
+
+    prepend_div.appendChild(span)
+
+    const input = document.createElement('input')
+    input.setAttribute('type', 'text')
+    input.setAttribute('class', 'form-control')
+    input.setAttribute('placeholder', 'new rack name')
+
+    input_div.appendChild(prepend_div)
+    input_div.appendChild(input)
+
+    header.appendChild(input_div)
+    console.log(edit_btn)
+    console.log(add_btn)
+}
+
+const cancelEdit = function() {
+    
+}
+
+const deleteRack = function(rack) {
+    if (rack['shelves'].length != 0) {
+        alert('There are shelves on this rack. Delete the shelves before deleting rack.')
+        return
+    }
+    fetch('/config/rack/' + rack['_id'], {method: 'DELETE'})
+        .then(response => response.json())
+        .then(response => {
+            const root = document.getElementById(response['_id']+'-root')
+            rack_div.removeChild(root)
+        })
+        .catch(err => alert('unable to delete rack ' + err))
+}
+
+const addNewShelf = function(rackId, body, options_div) {
+    const options = {
+        method: 'POST',
+        body: new URLSearchParams({rackId: rackId})
+    };
+    
+    fetch('/config/shelf', options)
+    .then(response => response.json())
+    .then(shelf => {
+        const shelf_root = create_shelf(shelf)
+        body.insertBefore(shelf_root, options_div)
+    })
+    .catch(err => alert('unable to add new shelf ' + err));
 }
 
--- server/controllers/rack.js
+++ server/controllers/rack.js
@@ -1,48 +1,65 @@
 const rack = require('../models/rackModel')
 
-
 const newRack = async (req, res) => {
+    try {
+        console.log('Creating new rack')
 
-    const newRack = new rack({
-        name: 'sample rack',
-        shelves: []
-    })
-    await newRack.save();
+        const newRack = new rack({
+            name: 'sample rack',
+            shelves: []
+        })
+        await newRack.save();
 
-    return res.status(201).json(newRack)
+        return res.status(201).json(newRack)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({'error': 'exception thrown'})
+    }
 }
 
 const updateName = async (req, res) => {
-    if (!req.body._id) 
-        return res.status(400).json({'error': 'missing pwm _id'})
-    
-    if (!req.body.name) 
-        return res.status(400).json({'error': 'missing name'})
-    
-    
-    const doc = await rack.findById(req.body._id)
+    try {
+        console.log('Updating rack name. body=' + req.body)
+        if (!req.body._id) 
+            return res.status(400).json({'error': 'missing pwm _id'})
+        
+        if (!req.body.name) 
+            return res.status(400).json({'error': 'missing name'})
+        
+        
+        const doc = await rack.findById(req.body._id)
 
-    if (!doc)
-        return res.status(400).json({'error': 'corresponding _id does not exist'})
-    
-    doc.name = req.body.name
-    doc.save()
-    
-    return res.status(201).json(doc)
+        if (!doc)
+            return res.status(400).json({'error': 'corresponding _id does not exist'})
+        
+        doc.name = req.body.name
+        doc.save()
+        
+        return res.status(201).json(doc)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({'error': 'exception thrown'})
+    }
 }
 
 const deleteRack = async (req, res) => {
-    const doc = await rack.findById(req.params.rackId)
+    try {
+        console.log('Deleting rack. params=' + req.params)
+        const doc = await rack.findById(req.params.rackId)
 
-    if (!doc)
-        return res.status(400).json({'error': 'corresponding rackId does not exist'})
+        if (!doc)
+            return res.status(400).json({'error': 'corresponding rackId does not exist'})
 
-    if (doc.shelves.length != 0)
-        return res.status(400).json({'error': 'rack contains shelves'})
+        if (doc.shelves.length != 0)
+            return res.status(400).json({'error': 'rack contains shelves'})
 
-    doc.remove()
+        doc.remove()
 
-    return res.status(201).json({doc})
+        return res.status(201).json(doc)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({'error': 'exception thrown'})
+    }
 }
 
 module.exports = {
