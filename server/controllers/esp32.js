f66e0fac93608a1b26b832ad0f39f87f7d166346
tshetrim <tshetrim123@gmail.com>
2022-07-05 10:36:07 -0400

    add more controls to dashboard

--- server/controllers/esp32.js
+++ server/controllers/esp32.js
@@ -1,64 +1,109 @@
 const path = require("path");
-const esp32 = require('../models/esp32Model')
+const esp32 = require("../models/esp32Model");
 
-const reset = async(req, res) => {
-    const docs = await esp32.find()
+const reset = async (req, res) => {
+	const docs = await esp32.find();
 
-    if (docs.length == 0) {
-        const newEsp32 = new esp32({
-            tod: '00:00:00',
-            sunrise: '08:00',
-            sunset: '20:00',
-            duration: 30,
-            rValue: 0,
-            gValue: 0,
-            bValue: 0
-        })
-        await newEsp32.save();
-        return res.status(201).json(newEsp32)
-    }
+	if (docs.length == 0) {
+		const newEsp32 = new esp32({
+			tod: "00:00:00",
+			sunrise: "08:00",
+			sunset: "20:00",
+			duration: 30,
+			rValue: 0,
+			gValue: 0,
+			bValue: 0,
+		});
+		await newEsp32.save();
+		return res.status(201).json(newEsp32);
+	}
 
-    return res.status(201).json(docs[0])
-}
+	return res.status(201).json(docs[0]);
+};
 
-const configs = async(req, res) => {
-    const docs = await esp32.find()
-    const doc = docs[0] 
-    const today = new Date();
-    doc['tod'] =  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    return res.status(201).json(doc)
-}
+const config = async (req, res) => {
+	const docs = await esp32.find();
+	const doc = docs[0];
+	const today = new Date();
+	//converting to utc time
+	const utc = new Date(today.toUTCString().slice(0, -4));
 
-const dashboard = async(req, res) => {
-    res.sendFile(path.resolve(__dirname, "../public/esp32.html"));
-}
+	//doc["tod"] = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+	//today.setHours(today.getHours());
+	doc["tod"] = utc.getHours() + ":" + utc.getMinutes() + ":" + utc.getSeconds();
+
+	return res.status(201).json(doc);
+};
+
+const dashboard = async (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../public/esp32.html"));
+};
 
 const save = async (req, res) => {
+	console.log("incoming data");
+	console.log(JSON.stringify(req.body));
 
-    console.log('incoming data')
-    console.log(JSON.stringify(req.body))
+	if (!req.body.pin || !req.body.value)
+		return res.status(400).json({ error: "missing pin / value" });
 
-    if (!req.body.pin || !req.body.value) 
-        return res.status(400).json({'error': 'missing pin / value'})
+	const pin = req.body.pin;
+	if (pin != "r" && pin != "g" && pin != "b")
+		return res.status(400).json({ error: "invalid pin" });
 
-    const pin = req.body.pin
-    if (pin != 'r' && pin != 'g' && pin != 'b')
-        return res.status(400).json({'error': 'invalid pin'})
+	const docs = await esp32.find();
+	console.log(docs);
+	const doc = docs[0];
+	console.log(doc);
 
-    const docs = await esp32.find()
-    const doc = docs[0] 
+	doc[pin + "Value"] = req.body.value;
 
-    doc[pin+'Value'] = req.body.value
+	await doc.save();
 
-    await doc.save()
+	return res.status(201).json(doc);
+};
 
-    return res.status(201).json(doc)
-}
+const saveDuration = async (req, res) => {
+	console.log("incoming data");
+	console.log(JSON.stringify(req.body));
 
+	if (!req.body.duration) return res.status(400).json({ error: "missing duration" });
+
+	const docs = await esp32.find();
+	const doc = docs[0];
+
+	doc["duration"] = req.body.duration;
+
+	await doc.save();
+
+	return res.status(201).json(doc);
+};
+
+const saveCustom = async (req, res) => {
+	console.log("incoming data");
+	console.log(JSON.stringify(req.body));
+
+	if (!req.body.sunrise || !req.body.sunset)
+		return res.status(400).json({ error: "missing sunrise / sunset" });
+
+	const docs = await esp32.find();
+	console.log(docs);
+	const doc = docs[0];
+	console.log(doc);
+
+	doc["sunrise"] = req.body.sunrise;
+	doc["sunset"] = req.body.sunset;
+
+	await doc.save();
+
+	return res.status(201).json(doc);
+};
 
 module.exports = {
-    reset,
-    configs,
-    dashboard,
-    save
-}
+	reset,
+	config,
+	dashboard,
+	save,
+	saveDuration,
+	saveCustom,
+};
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,56 +2,58 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 
-const apiController = require('../controllers/api') 
-const pwmController = require('../controllers/pwm') 
-const rackController = require('../controllers/rack')
-const shelfController = require('../controllers/shelf')
-const esp32Controller = require('../controllers/esp32')
+const apiController = require("../controllers/api");
+const pwmController = require("../controllers/pwm");
+const rackController = require("../controllers/rack");
+const shelfController = require("../controllers/shelf");
+const esp32Controller = require("../controllers/esp32");
 
 /* Serves HTML files to user */
-router.get('/', authController.index)
-router.post('/login', authController.login)
-router.get('/logout', authController.logout)
+router.get("/", authController.index);
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
 
-router.get('/admin', authController.adminAuth)
-router.get('/admin', authController.admin)
-router.get('/adminOLD', authController.adminOLD)
+router.get("/admin", authController.adminAuth);
+router.get("/admin", authController.admin);
+router.get("/adminOLD", authController.adminOLD);
 
 /* Admin and Student interaction with shelf values */
-router.put('/api/shelf/mode', authController.dashboard, apiController.modeChange)
-router.put('/api/shelf/value', authController.dashboard, apiController.valueChange)
+router.put("/api/shelf/mode", authController.dashboard, apiController.modeChange);
+router.put("/api/shelf/value", authController.dashboard, apiController.valueChange);
 
 /* forces all admin api calls to be fully authenticated */
-router.all('/config/*', authController.adminAuth)
+router.all("/config/*", authController.adminAuth);
 
 /* Admin interaction with PWM module */
-router.post('/config/pwm', pwmController.newModule)
-router.put('/config/pwm/name', pwmController.updateName)
-router.put('/config/pwm/dc', shelfController.dcShelf)
-router.put('/config/pwm/resync', pwmController.resync)
-router.delete('/config/pwm/:pwmId', pwmController.deleteModule)
+router.post("/config/pwm", pwmController.newModule);
+router.put("/config/pwm/name", pwmController.updateName);
+router.put("/config/pwm/dc", shelfController.dcShelf);
+router.put("/config/pwm/resync", pwmController.resync);
+router.delete("/config/pwm/:pwmId", pwmController.deleteModule);
 
 /* Admin interaction with Racks */
-router.post('/config/rack', rackController.newRack)
-router.put('/config/rack/name', rackController.updateName)
-router.delete('/config/rack/:rackId', rackController.deleteRack)
+router.post("/config/rack", rackController.newRack);
+router.put("/config/rack/name", rackController.updateName);
+router.delete("/config/rack/:rackId", rackController.deleteRack);
 
 /* Admin interaction with Shelves */
-router.post('/config/shelf', shelfController.newShelf)
-router.put('/config/shelf/name', shelfController.updateName)
-router.put('/config/shelf/addr', shelfController.updateAddr)
-router.delete('/config/shelf/:shelfId', shelfController.deleteShelf)
+router.post("/config/shelf", shelfController.newShelf);
+router.put("/config/shelf/name", shelfController.updateName);
+router.put("/config/shelf/addr", shelfController.updateAddr);
+router.delete("/config/shelf/:shelfId", shelfController.deleteShelf);
 
 /* Available to public, no credentials */
-router.get('/api/config', apiController.config)
+router.get("/api/config", apiController.config);
 
 /* Client requesting server modification */
-router.put('/api/config/syncd', authController.clientAuth, apiController.syncdNotify)
+router.put("/api/config/syncd", authController.clientAuth, apiController.syncdNotify);
 
 /* ESP32 Project */
-router.get('/esp32/reset', esp32Controller.reset)
-router.get('/esp32/config', esp32Controller.configs)
-router.get('/esp32/dashboard', esp32Controller.dashboard)
-router.post('/esp32/save', esp32Controller.save)
+router.get("/esp32/reset", esp32Controller.reset);
+router.get("/esp32/config", esp32Controller.config);
+router.get("/esp32/dashboard", esp32Controller.dashboard);
+router.post("/esp32/save", esp32Controller.save);
+router.post("/esp32/saveDuration", esp32Controller.saveDuration);
+router.post("/esp32/saveCustom", esp32Controller.saveCustom);
 
-module.exports = router
+module.exports = router;
